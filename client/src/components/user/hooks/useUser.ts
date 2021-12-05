f54b61c239cd58aa9345911b581d4c8a3444daf5
spiism <sp.iism@gmail.com>
2021-12-05 13:15:03 +1300

    integrate Auth with React Query

--- client/src/components/user/hooks/useUser.ts
+++ client/src/components/user/hooks/useUser.ts
@@ -1,21 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
-import type { User } from '../../../../../shared/types';
-import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
+import type { User } from "../../../../../shared/types";
+import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
+import { queryKeys } from "../../../react-query/constants";
 import {
   clearStoredUser,
   getStoredUser,
   setStoredUser,
-} from '../../../user-storage';
+} from "../../../user-storage";
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data } = await axiosInstance.get(`/user/${user.id}`, {
-//     headers: getJWTHeader(user),
-//   });
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data } = await axiosInstance.get(`/user/${user.id}`, {
+    headers: getJWTHeader(user),
+  });
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -24,30 +25,30 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  const [user, setUser] = useState<User | null>(getStoredUser());
+  // const [user, setUser] = useState<User | null>(getStoredUser());
 
+  const queryClient = useQueryClient();
   // TODO: call useQuery to update user data from server
+  const { data: user } = useQuery(queryKeys.user, () => getUser(user));
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
     // set user in state
-    setUser(newUser);
-
-    // update user in localstorage
-    setStoredUser(newUser);
-
+    // setUser(newUser);
+    // // update user in localstorage
+    // setStoredUser(newUser);
     // TODO: pre-populate user profile in React Query client
+    queryClient.setQueryData(queryKeys.user, newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
     // update state
-    setUser(null);
-
-    // remove from localstorage
-    clearStoredUser();
-
+    // setUser(null);
+    // // remove from localstorage
+    // clearStoredUser();
     // TODO: reset user to null in query client
+    queryClient.setQueryData(queryKeys.user, null);
   }
 
   return { user, updateUser, clearUser };
